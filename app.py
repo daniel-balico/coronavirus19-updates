d16a41130666e373018909649ccd18582b9d3dc2
Daniel Shan Balico <danielshan.balico@gmail.com>
2020-05-09 13:07:46 +0800

    Added a table view and fix code structure

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from datetime import datetime
 import os
+import urllib.request as urlrequest
+import json
 
 app = Flask(__name__)
 
@@ -22,9 +24,21 @@ def main():
 	return render_template('main.html', 
 		countries=country_list, url_country=get_country, date=date)
 
+@app.route('/tableview')
+def table():
+	with urlrequest.urlopen('https://coronavirus-19-api.herokuapp.com/countries') as response:
+		source = response.read()
+		data = json.loads(source)
+	#Read all countries in the text file
+	country_list_file = open('countries.txt', 'r')
+	country_list = country_list_file.readlines()
+	#get current date
+	date = datetime.today().strftime('%Y-%m-%d')
+	return render_template('table.html', countries=country_list, date=date, jsonData=data, table=True)
+
 @app.route('/sitemap.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
