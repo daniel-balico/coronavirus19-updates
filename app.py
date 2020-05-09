34a5bd50dcd2931162a8d7c7fd264783fc384b8a
Daniel Shan Balico <danielshan.balico@gmail.com>
2020-05-09 08:44:52 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, url_for, render_template
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error-404.html')
+
+@app.route('/')
+def main():
+	#Read all countries in the text file
+	country_list_file = open('countries.txt', 'r')
+	country_list = country_list_file.readlines()
+	#Get the current country in the url parameter
+	get_country = request.args.get('country', default='World')
+	#get current date
+	date = datetime.today().strftime('%Y-%m-%d')
+
+	return render_template('main.html', 
+		countries=country_list, url_country=get_country, date=date)
+
+if __name__ == '__main__':
+	app.run()
